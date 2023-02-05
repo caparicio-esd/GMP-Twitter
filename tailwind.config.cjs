461e78720c5ddd68402ae40ae384f767af4d71d7
caparicio-esd <caparicio@esdmadrid.es>
2023-02-05 13:52:33 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/**/*.{scss}",  "./app/**/*.{js}"],
+  theme: {
+    fontFamily: {
+      sans: ["'Source Sans Pro'", "sans-serif"]
+    },
+    fontSize: {
+      base: ".95rem"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
